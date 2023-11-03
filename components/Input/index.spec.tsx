406b52c16aa8ed08b5d127ab56e4c17f6c463d49
imsugeno <g.tokyo.kazusa@gmail.com>
2023-11-04 00:00:09 +0900

    デバッグする

---
+++ components/Input/index.spec.tsx
@@ -0,0 +1,20 @@
+import { render, screen, RenderResult } from '@testing-library/react';
+import { Input } from './index';
+
+describe('Input', () => {
+    let renderResult: RenderResult
+
+    beforeEach(() => {
+        renderResult = render(<Input label="username" id="username" />)
+    })
+
+    afterEach(() => {
+        renderResult.unmount()
+    })
+
+    it('should empty in input on initial render', () => {
+        const inputNode = screen.getByLabelText('username') as HTMLInputElement
+
+        expect(inputNode).toHaveValue('')
+    })
+})
